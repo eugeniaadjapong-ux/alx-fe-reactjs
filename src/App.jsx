7d05c37df8caa1fa82f75af23447cb695a5bbfed
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2026-02-03 01:53:18 +0000

    Clean App.jsx: integrate WelcomeMessage and Task 3 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+// Task 2 Component
 import WelcomeMessage from './components/WelcomeMessage';
 
+// Task 3 Components
+import Header from './specific/Header';
+import MainContent from './specific/MainContent';
+import Footer from './specific/Footer';
+
 function App() {
-    return (
-        <div>
-            <WelcomeMessage />
-        </div>
-    );
+  return (
+    <div>
+      {/* Task 2: JSX Practice */}
+      <WelcomeMessage />
+
+      {/* Task 3: Specific Components */}
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
