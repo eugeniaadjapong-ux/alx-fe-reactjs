946d13854557af7249115beb5d3c88a0c4c68b08
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2026-02-07 13:46:30 +0000

    Complete Company Website with react

---
+++ my-company/src/App.jsx
@@ -0,0 +1,19 @@
+import Home from './components/home';
+import About from './components/about';
+import Services from './components/services';
+import Contact from './components/contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
