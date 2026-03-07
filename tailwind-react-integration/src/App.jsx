815aeeffbbb2cd3d3cb6b6ad36c9e3da13b5f41e
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2026-03-07 04:55:00 +0000

    Styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Tailwind is working!
-      </h1>
+    <div>
+      <UserProfile />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
