6144d84818928854fbcb34fc3bcfac49cf25cad3
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2026-03-07 06:07:18 +0000

    Fix Tailwind setup

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-blue-500 text-3xl font-bold">
         Recipe Sharing Platform
       </h1>
